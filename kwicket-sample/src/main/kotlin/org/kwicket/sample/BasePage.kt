6fe5bd2a631efdb9b4add80c3721d82405d97874
ageery <andrew_geery@windsorsolutions.com>
2018-01-23 05:52:00 -0500

    Cleaned up warnings.

--- kwicket-sample/src/main/kotlin/org/kwicket/sample/BasePage.kt
+++ kwicket-sample/src/main/kotlin/org/kwicket/sample/BasePage.kt
@@ -31,7 +31,6 @@ abstract class BasePage(pageParameters: PageParameters? = null) : WebPage(pagePa
     private val feedback: Component
 
     init {
-        initModel()
         q(HtmlTag("html"))
         q(Label("title", ResourceModel("page.title")))
         q(MobileViewportMetaTag("viewport"))
--- kwicket-wicket-bootstrap-core/src/main/kotlin/org/kwicket/agilecoders/wicket/core/ajax/markup/html/bootstrap/navbar/Navbars.kt
+++ kwicket-wicket-bootstrap-core/src/main/kotlin/org/kwicket/agilecoders/wicket/core/ajax/markup/html/bootstrap/navbar/Navbars.kt
@@ -1,11 +1,12 @@
 package org.kwicket.agilecoders.wicket.core.ajax.markup.html.bootstrap.navbar
 
+import de.agilecoders.wicket.core.markup.html.bootstrap.navbar.INavbarComponent
 import de.agilecoders.wicket.core.markup.html.bootstrap.navbar.Navbar
 import de.agilecoders.wicket.core.markup.html.bootstrap.navbar.NavbarComponents
 import org.apache.wicket.Component
 
-fun positionLeft(vararg components: Component) =
+fun positionLeft(vararg components: Component): List<INavbarComponent> =
     NavbarComponents.transform(Navbar.ComponentPosition.LEFT, *components)
 
-fun positionRight(vararg components: Component) =
+fun positionRight(vararg components: Component): List<INavbarComponent> =
     NavbarComponents.transform(Navbar.ComponentPosition.RIGHT, *components)
