d0924c151fe71b2c1d8ffce1aced65c11676f21c
ageery <andrew_geery@windsorsolutions.com>
2018-01-29 09:45:50 -0500

    Do not expliclty set the Spring context.

--- kwicket-wicket-spring/src/main/kotlin/org/kwicket/wicket/spring/Applications.kt
+++ kwicket-wicket-spring/src/main/kotlin/org/kwicket/wicket/spring/Applications.kt
@@ -2,14 +2,8 @@ package org.kwicket.wicket.spring
 
 import org.apache.wicket.protocol.http.WebApplication
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector
-import org.springframework.web.context.support.WebApplicationContextUtils
 
 fun <A : WebApplication> A.enableSpringIoC(): A {
-    componentInstantiationListeners.add(
-        SpringComponentInjector(
-            this,
-            WebApplicationContextUtils.getWebApplicationContext(servletContext)
-        )
-    )
+    componentInstantiationListeners.add(SpringComponentInjector(this))
     return this
 }
