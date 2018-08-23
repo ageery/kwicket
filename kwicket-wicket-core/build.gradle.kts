9e6f5cc38533e73bc0fe61f33c5cb46f48c54add
ageery <andrew_geery@windsorsolutions.com>
2018-08-23 07:05:34 -0400

    Cleaned up version numbers.

--- kwicket-wicket-core/build.gradle.kts
+++ kwicket-wicket-core/build.gradle.kts
@@ -1,6 +1,8 @@
-// FIXME: pull the version numbers into vals...
+import org.kwicket.gradle.Versions.wicketVersion
+import org.kwicket.gradle.Versions.servletApiVersion
+
 dependencies {
     compile(project(":kwicket-core"))
-    compileOnly("javax.servlet:javax.servlet-api:3.1.0")
-    compileOnly("org.apache.wicket:wicket-core:8.0.0-M9")
+    compileOnly("javax.servlet:javax.servlet-api:$servletApiVersion")
+    compileOnly("org.apache.wicket:wicket-core:$wicketVersion")
 }
